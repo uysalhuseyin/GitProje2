9d4cef752166a02a8b2ac3d0df7ebaebf920fbe4
QA <uysalhuseyin1995@gmail.com>
2022-07-05 17:42:34 +0300

    second mission done

--- src/student2.java
+++ src/student2.java
@@ -1,9 +1,7 @@
-public class student {
+public class student2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World 2");
         System.out.println("Hello World 3");
-
     }
-
 }
